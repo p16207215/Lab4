b5eba21a93db2a1c25ad8607a0e4a8ff36670ebb
Priyansh shah <shahpriyansh74@gmail.com>
2018-10-29 12:39:36 +0000

    Updated the lab4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 console.log("hello");
+
+const menuToggle  = document.getElementById("menu-toggle");
+const menuNav = document.getElementById("menu-nav");
+
+ toggleMenu => console.log("Called Toggle menu");
+menuToggle.addEventListener('click', toggleMenu());
