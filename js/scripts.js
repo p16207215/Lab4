26f879f7a9d7722bf129c510ce2719757c032f09
Priyansh shah <shahpriyansh74@gmail.com>
2018-10-29 14:22:29 +0000

    Latest Updated files

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,5 @@ console.log("hello");
 const menuToggle  = document.getElementById("menu-toggle");
 const menuNav = document.getElementById("menu-nav");
 
- toggleMenu => console.log("Called Toggle menu");
+ const toggleMenu = () => console.log("Called Toggle menu");
 menuToggle.addEventListener('click', toggleMenu());
